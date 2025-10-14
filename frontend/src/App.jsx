b3766bf6fafd6265c81074e9be4e6b12add53261
mungaiHub <amosmungai085@gmail.com>
2025-10-14 18:24:10 +0300

    Initial commit: set up Food Delivery project with navbar and assets

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+
+const App = () => {
+  return (
+    <div ClassName='app'>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
