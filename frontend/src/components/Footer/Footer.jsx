ec19d8f9773270e609c3622cd6b18f90dbb6d662
mungaiHub <amosmungai085@gmail.com>
2025-11-13 13:35:02 +0300

    changed the logo

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
     <div className='footer' id='footer'>
         <div className="footer-content">
             <div className="footer-content-left">
-                <img src={assets.logo} alt="" />
+                <img src={assets.logo} alt=""  width={"100px"}/>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, nemo.</p>
                 <div className="footer-social-icons">
-                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.facebook_icon} alt=""  />
                     <img src={assets.twitter_icon} alt="" />
                     <img src={assets.linkedin_icon} alt="" />
                 </div>
